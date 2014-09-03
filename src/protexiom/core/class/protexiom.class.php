d3e6260a9011078036c956592e8debd2cb989d52
Fdp1 <fdp1nm@gmail.com>
2014-09-03 21:48:21 +0200

    Creation d'un plugin de base sans commande

---
+++ src/protexiom/core/class/protexiom.class.php
@@ -0,0 +1,384 @@
+<?php
+
+/* This file is part of the Jeedom Protexiom plugin.
+ *
+ * This plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+
+class protexiom extends eqLogic {
+    /*     * *************************Attributs****************************** */
+
+    private $_UpdateDate = '';
+    private $_HwVersion = '';
+    private $_SomfyAuthCookie = '';
+    private $_SomfyHost = '';
+    private $_SomfyPort = '';
+    private $_WebProxyHost = '';
+    private $_WebProxyPort = '';
+
+    /*     * ***********************Methode static*************************** */
+
+    /*public static function pull($_options) {
+        $weather = weather::byId($_options['weather_id']);
+        if (is_object($weather)) {
+            $weather_xml = $weather->getWeatherFromYahooXml();
+            $sunrise = $weather_xml['astronomy']['sunrise'];
+            $sunset = $weather_xml['astronomy']['sunset'];
+            if ((date('Hi') + 100) >= $sunrise && (date('Hi') + 100 ) < $sunset) {
+                $search = 'sunrise';
+            } else {
+                $search = 'sunset';
+            }
+            foreach ($weather->getCmd() as $cmd) {
+                if ($cmd->getConfiguration('data') == $search) {
+                    $cmd->event(date('Hi'));
+                }
+            }
+            $weather->reschedule();
+        } else {
+            $cron = cron::byClassAndFunction('weather', 'pull', $_options);
+            if (is_object($cron)) {
+                $cron->remove();
+            }
+            throw new Exception('Weather ID non trouvé : ' . $_options['weather_id'] . '. Tache supprimé');
+        }
+    }*/
+
+    /*public static function cronHourly() {
+        foreach (self::byType('weather') as $weather) {
+            if ($weather->getIsEnable() == 1) {
+                $cron = cron::byClassAndFunction('weather', 'pull', array('weather_id' => intval($weather->getId())));
+                if (!is_object($cron)) {
+                    $weather->reschedule();
+                } else {
+                    $c = new Cron\CronExpression($cron->getSchedule(), new Cron\FieldFactory);
+                    try {
+                        $c->getNextRunDate();
+                    } catch (Exception $ex) {
+                        $weather->reschedule();
+                    }
+                }
+            }
+        }
+    }*/
+
+    /*     * *********************Methode d'instance************************* */
+
+    /*public function preUpdate() {
+        if ($this->getConfiguration('city') == '') {
+            throw new Exception(__('L\identifiant de la ville ne peut être vide', __FILE__));
+        }
+        $this->setCategory('heating', 1);
+    }*/
+
+    /*public function postInsert() {
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Température', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '-1');
+        $weatherCmd->setConfiguration('data', 'temp');
+        $weatherCmd->setUnite('°C');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Humidité', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '-1');
+        $weatherCmd->setConfiguration('data', 'humidity');
+        $weatherCmd->setUnite('%');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Pression', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '-1');
+        $weatherCmd->setConfiguration('data', 'pressure');
+        $weatherCmd->setUnite('Pa');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Condition Actuelle', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '-1');
+        $weatherCmd->setConfiguration('data', 'condition');
+        $weatherCmd->setUnite('');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('string');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Vitesse du vent', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '-1');
+        $weatherCmd->setConfiguration('data', 'wind_speed');
+        $weatherCmd->setUnite('km/h');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Direction du vent', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '-1');
+        $weatherCmd->setConfiguration('data', 'wind_direction');
+        $weatherCmd->setUnite('');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('string');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Lever du soleil', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '-1');
+        $weatherCmd->setConfiguration('data', 'sunrise');
+        $weatherCmd->setUnite('');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Coucher du soleil', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '-1');
+        $weatherCmd->setConfiguration('data', 'sunset');
+        $weatherCmd->setUnite('');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Température Min', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '0');
+        $weatherCmd->setConfiguration('data', 'low');
+        $weatherCmd->setUnite('°C');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Température Max', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '0');
+        $weatherCmd->setConfiguration('data', 'high');
+        $weatherCmd->setUnite('°C');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Condition', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '0');
+        $weatherCmd->setConfiguration('data', 'condition');
+        $weatherCmd->setUnite('');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('string');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Température Min +1', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '1');
+        $weatherCmd->setConfiguration('data', 'low');
+        $weatherCmd->setUnite('°C');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Température Max +1', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '1');
+        $weatherCmd->setConfiguration('data', 'high');
+        $weatherCmd->setUnite('°C');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('numeric');
+        $weatherCmd->save();
+
+        $weatherCmd = new weatherCmd();
+        $weatherCmd->setName(__('Condition +1', __FILE__));
+        $weatherCmd->setEqLogic_id($this->id);
+        $weatherCmd->setConfiguration('day', '1');
+        $weatherCmd->setConfiguration('data', 'condition');
+        $weatherCmd->setUnite('');
+        $weatherCmd->setType('info');
+        $weatherCmd->setSubType('string');
+        $weatherCmd->save();
+    }*/
+
+    /*public function postSave() {
+        $this->reschedule();
+    }*/
+
+    /*public function preRemove() {
+        $cron = cron::byClassAndFunction('weather', 'pull', array('weather_id' => intval($this->getId())));
+        if (is_object($cron)) {
+            $cron->remove();
+        }
+    }*/
+
+    /*public function toHtml($_version = 'dashboard') {
+        if ($this->getIsEnable() != 1) {
+            return '';
+        }
+        $_version = jeedom::versionAlias($_version);
+        $weather = $this->getWeatherFromYahooXml();
+        if (!is_array($weather)) {
+            $replace = array(
+                '#icone#' => '',
+                '#id#' => $this->getId(),
+                '#city#' => '',
+                '#condition#' => '{{Impossible de récupérer la météo.Pas d\'internet ?}}',
+                '#temperature#' => '',
+                '#windspeed#' => '',
+                '#humidity#' => '',
+                '#pressure#' => '',
+                '#sunrise#' => '',
+                '#sunset#' => '',
+                '#collectDate#' => '',
+                '#background_color#' => $this->getBackgroundColor($_version),
+                '#eqLink#' => $this->getLinkToConfiguration(),
+                '#forecast#' => '',
+            );
+            return template_replace($replace, getTemplate('core', $_version, 'current', 'weather'));
+        }
+        $html_forecast = '';
+        $forcast_template = getTemplate('core', $_version, 'forecast', 'weather');
+        foreach ($weather['forecast'] as $forecast) {
+            $replace = array(
+                '#day#' => $forecast['day'],
+                '#icone#' => $forecast['icone'],
+                '#low_temperature#' => $forecast['low_temperature'],
+                '#hight_temperature#' => $forecast['high_temperature'],
+            );
+            $html_forecast .= template_replace($replace, $forcast_template);
+        }
+        $replace = array(
+            '#id#' => $this->getId(),
+            '#icone#' => $weather['condition']['icone'],
+            '#city#' => $weather['location']['city'],
+            '#condition#' => $weather['condition']['text'],
+            '#temperature#' => $weather['condition']['temperature'],
+            '#windspeed#' => $weather['wind']['speed'],
+            '#humidity#' => $weather['atmosphere']['humidity'],
+            '#pressure#' => $weather['atmosphere']['pressure'],
+            '#sunrise#' => $weather['astronomy']['sunrise'],
+            '#sunset#' => $weather['astronomy']['sunset'],
+            '#collectDate#' => $this->getCollectDate(),
+            '#background_color#' => $this->getBackgroundColor($_version),
+            '#eqLink#' => $this->getLinkToConfiguration(),
+            '#forecast#' => $html_forecast,
+        );
+        return template_replace($replace, getTemplate('core', $_version, 'current', 'weather'));
+    }*/
+
+    /*public function getShowOnChild() {
+        return true;
+    }*/
+
+    /*     * **********************Getteur Setteur*************************** */
+
+    /*public function getCollectDate() {
+        return $this->_collectDate;
+    }*/
+
+    /*public function setCollectDate($_collectDate) {
+        $this->_collectDate = $_collectDate;
+    }*/
+
+}
+
+class protexiomCmd extends cmd {
+    /*     * *************************Attributs****************************** */
+
+
+    /*     * ***********************Methode static*************************** */
+
+
+    /*     * *********************Methode d'instance************************* */
+
+    /*
+    public function dontRemoveCmd() {
+        return true;
+    }
+     */
+
+    public function execute($_options = array()) {
+        return false;
+        /*
+        $eqLogic_weather = $this->getEqLogic();
+        $weather = $eqLogic_weather->getWeatherFromYahooXml();
+
+        if (!is_array($weather)) {
+            sleep(1);
+            $weather = $eqLogic_weather->getWeatherFromYahooXml();
+            if (!is_array($weather)) {
+                return false;
+            }
+        }
+
+        if ($this->getConfiguration('day') == -1) {
+            if ($this->getConfiguration('data') == 'condition') {
+                return $weather['condition']['text'];
+            }
+            if ($this->getConfiguration('data') == 'temp') {
+                return $weather['condition']['temperature'];
+            }
+            if ($this->getConfiguration('data') == 'humidity') {
+                return $weather['atmosphere']['humidity'];
+            }
+            if ($this->getConfiguration('data') == 'pressure') {
+                return $weather['atmosphere']['pressure'];
+            }
+            if ($this->getConfiguration('data') == 'wind_speed') {
+                return $weather['wind']['speed'];
+            }
+            if ($this->getConfiguration('data') == 'wind_direction') {
+                return $weather['wind']['direction'];
+            }
+            if ($this->getConfiguration('data') == 'sunrise') {
+                return $weather['astronomy']['sunrise'];
+            }
+            if ($this->getConfiguration('data') == 'sunset') {
+                return $weather['astronomy']['sunset'];
+            }
+        }
+
+        if ($this->getConfiguration('data') == 'condition') {
+            return $weather['forecast'][$this->getConfiguration('day')]['condition'];
+        }
+        if ($this->getConfiguration('data') == 'low') {
+            return $weather['forecast'][$this->getConfiguration('day')]['low_temperature'];
+        }
+        if ($this->getConfiguration('data') == 'high') {
+            return $weather['forecast'][$this->getConfiguration('day')]['high_temperature'];
+        }
+        return false;
+        */
+    }
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+?>
---
+++ src/protexiom/desktop/php/protexiom.php
@@ -0,0 +1,157 @@
+<?php
+if (!isConnect('admin')) {
+    throw new Exception('{{401 - Accès non autorisé}}');
+}
+sendVarToJS('eqType', 'protexiom');
+?>
+
+<div class="row row-overflow">
+    <div class="col-md-2">
+        <div class="bs-sidebar">
+            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une Protexiom}}</a>
+                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+                <?php
+                foreach (eqLogic::byType('protexiom') as $eqLogic) {
+                    echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName() . '</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+    <div class="col-md-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+        <form class="form-horizontal">
+            <fieldset>
+                <legend>{{Général}}</legend>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{Nom de la centrale Somfy Protexiom}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                        <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de la centrale Somfy Protexiom}}"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label" >{{Objet parent}}</label>
+                    <div class="col-md-3">
+                        <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                            <option value="">{{Aucun}}</option>
+                            <?php
+                            foreach (object::all() as $object) {
+                                echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label" >{{Activer}}</label>
+                    <div class="col-md-1">
+                        <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" size="16" checked/>
+                    </div>
+                    <label class="col-md-2 control-label" >{{Visible}}</label>
+                    <div class="col-md-1">
+                        <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{Adresse IP ou Hostname:port}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="SomfyHostPort" placeholder="{{Adresse IP ou Hostname:port}}"/>
+									{{Exemple}}: alarme.mondomaine.com:80 {{ou}} 192.168.1.253:80 {{ou}} 192.1681.253
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-2 control-label" >{{SSL Enabled}}</label>
+                    <div class="col-md-1">
+                        <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="SSLEnabled" placeholder="{{SSL Enabled}}" size="16" checked/>
+									{{SSL PAS ENCORE SUPPORTE. Ne pas activer.}}
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{User Password}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="UserPwd" placeholder="{{User Password}}"/>
+									{{Exemple}}: s3cr3tPassw0rd
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{AuthCard Line 1}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="AuthCardL1" placeholder="{{AuthCard Line 1}}"/>
+									{{Exemple}}: 1234|5678|9012|3456|7890|1234
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{AuthCard Line 2}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="AuthCardL2" placeholder="{{AuthCard Line 2}}"/>
+									{{Exemple}}: 1234|5678|9012|3456|7890|1234
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{AuthCard Line 3}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="AuthCardL3" placeholder="{{AuthCard Line 3}}"/>
+									{{Exemple}}: 1234|5678|9012|3456|7890|1234
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{AuthCard Line 4}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="AuthCardL4" placeholder="{{AuthCard Line 4}}"/>
+									{{Exemple}}: 1234|5678|9012|3456|7890|1234
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{AuthCard Line 5}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="AuthCardL5" placeholder="{{AuthCard Line 5}}"/>
+									{{Exemple}}: 1234|5678|9012|3456|7890|1234
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2 control-label">{{Proxy Hostname:port}}</label>
+                    <div class="col-md-3">
+                        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="WebProxyHostPort" placeholder="{{Proxy Hostname:port}}"/>
+									{{Optionnel (utilisé en debug avec Fiddler)}}. {{Exemple}}: alarme.mondomaine.com:80 {{or}} 192.168.1.253:80
+                    </div>
+                </div>
+           </fieldset> 
+        </form>
+
+<?php
+/* TODO
+* this would be the place to list protexiom cmd, for ease of use (documentation purpose only)
+* see weather.php line 68 as an exemple, and the printWeather js function in weather.js
+* this is a good idea, but low priority. let´s keep it in the TODO LIST for now
+
+        <legend>{{Protexiom}}</legend>
+        <div id="div_protexiom"></div>
+        <table id="table_protexiom" class="table table-bordered table-condensed">
+            <thead>
+                <tr>
+                    <th>{{Nom}}</th><th>{{Valeur}}</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+END TODO
+*/
+?>
+        <form class="form-horizontal">
+            <fieldset>
+                <div class="form-actions">
+                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+                </div>
+            </fieldset>
+        </form>
+
+    </div>
+</div>
+
+<?php include_file('desktop', 'protexiom', 'js', 'protexiom'); ?>
+<?php include_file('core', 'plugin.template', 'js'); ?>
