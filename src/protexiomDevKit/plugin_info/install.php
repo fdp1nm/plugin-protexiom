ad0175d4c452763c4ffd4a54f0a9793e19c23091
Fdp1 <fdp1nm@gmail.com>
2014-12-06 00:29:52 +0100

    Creation de la classe devkit

---
+++ src/protexiomDevKit/plugin_info/install.php
@@ -0,0 +1,45 @@
+<?php
+
+/* Copyright   2014 fdp1
+ *
+* This work is free. You can redistribute it and/or modify it under the
+* terms of the Do What The Fuck You Want To Public License, Version 2,
+* as published by Sam Hocevar. See the COPYING file for more details.
+*
+* This program is free software. It comes without any warranty, to
+* the extent permitted by applicable law. You can redistribute it
+* and/or modify it under the terms of the Do What The Fuck You Want
+* To Public License, Version 2, as published by Sam Hocevar. See
+* http://www.wtfpl.net/ for more details.
+*/
+
+require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+
+/**
+ * Called immedialty after plugin install
+ * Standard Jeedom function
+ * @author Fdp1
+*/
+/*function protexiomDevKit_install() {
+}*/
+
+
+/**
+ * Called immedialty after plugin update
+ * Standard Jeedom function
+ * @author Fdp1
+*/
+/*function protexiomDevKit_update() {
+    
+}*/
+
+/**
+ * Called before plugin update
+ * Standard Jeedom function
+ * @author Fdp1
+*/
+/*function protexiomDevKit_remove(){
+	
+}//End function protexiom_remove()*/
+
+?>
