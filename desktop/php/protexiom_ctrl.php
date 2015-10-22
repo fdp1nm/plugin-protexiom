4245e6ca1778c1d224b1c5b6e9ea62ec0e26daf1
Fdp1 <fdp1nm@gmail.com>
2015-10-22 23:28:36 +0200

    Ajout du champ commentaire sur le subDevice

--- desktop/php/protexiom_ctrl.php
+++ desktop/php/protexiom_ctrl.php
@@ -44,6 +44,13 @@
 					<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
 					<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
 					</div>
+                </div>
+				<div class="form-group">
+                    <label class="col-sm-2 control-label">{{Commentaire}}</label>
+                    <div class="col-sm-3">
+                    	<textarea class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commentaire"></textarea>
+                    </div>
+                </div>
             </fieldset> 
         </form>
 
