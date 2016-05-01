cfcc49d4abab024b76385e6952dbc57a07ab138a
Fdp1 <fdp1nm@gmail.com>
2016-05-01 17:29:29 +0200

    Supression des log debug précédement ajoutés

--- core/class/protexiom.class.php
+++ core/class/protexiom.class.php
@@ -849,14 +849,12 @@ class protexiom extends eqLogic {
      *
      */
 	public function toHtml($_version = 'dashboard') {
-		$this->log('debug', 'Running toHtml');
 		$replace = $this->preToHtml($_version);
 		if (!is_array($replace)) {
 			return $replace;
 		}
 		$version = jeedom::versionAlias($_version);
 		$cmd_html = '';
-		$this->log('debug', 'toHtml...Running throught cmds');
 		foreach ($this->getCmd() as $cmd) {
 			if($cmd->getIsVisible()){
 				if ($cmd->getLogicalId() == 'refresh') {
@@ -875,14 +873,10 @@ class protexiom extends eqLogic {
 			}
 		}
 		$replace['#cmd#'] = $cmd_html;
-		$this->log('debug', 'toHtml...Getting template');
 		self::$_templateArray[$version] = getTemplate('core', $version, 'eqLogic','protexiom');
-		$this->log('debug', 'toHtml...Template loaded');
 		
 		$html = template_replace($replace, self::$_templateArray[$version]);
-		$this->log('debug', 'toHtml...Caching widget');
 		cache::set('widgetHtml' . $_version . $this->getId(), $html, 0);
-		$this->log('debug', 'toHtml...Done');
 		return $html;
 	}//end toHtml function  
 
